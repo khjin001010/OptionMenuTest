9d901415f26bf2aec078bfa7f974644cda8bdb63
khjin001010 <khjin001010@naver.com>
2017-05-23 19:55:45 +0900

    #4 Event Handling

--- app/src/main/java/khjin001010/kr/hs/emirim/optionmenutest/MainActivity.java
+++ app/src/main/java/khjin001010/kr/hs/emirim/optionmenutest/MainActivity.java
@@ -1,16 +1,24 @@
 package khjin001010.kr.hs.emirim.optionmenutest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout linear;
+    Button but;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linear = (LinearLayout)findViewById(R.id.linear);
+        but = (Button)findViewById(R.id.but_ok);
     }
 
     @Override
@@ -20,6 +28,31 @@ public class MainActivity extends AppCompatActivity {
         mInfalter.inflate(R.menu.menu1, menu);
 
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.item_blue:
+                linear.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.item_red:
+                linear.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.item_yellow:
+                linear.setBackgroundColor(Color.YELLOW);
+                return true;
+            case R.id.item_but_rotate:
+                but.setRotation(but.getRotation()+15);
+                return true;
+            case R.id.item_but_size:
+                but.setScaleX(but.getScaleX()*1.5f); //1.5배(가로 방향)
+                but.setScaleY(but.getScaleY()*1.5f);
+                return true;
+        }
+
+        return false;
     }
 }
 
